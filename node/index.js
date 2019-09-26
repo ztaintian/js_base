4fbe41e474f0dac37d919ba8b0947413e779eca7
unknown <zhutiantian@ciicsh.local>
2019-09-26 16:51:32 +0800

    'hh'

--- node/index.js
+++ node/index.js
@@ -1,8 +1,16 @@
 const Koa = require('koa');
 const app = new Koa();
 app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   await next()
   ctx.response.type = "text/html";
-  ctx.response.body = "hello world"
+  if (ctx.path === '/') {
+    ctx.body = { code: 200, msg: 'success', data: { name: 'xxx' } }
+  } else if (ctx.path === '/aa') {
+    ctx.body = { code: 200, msg: 'success', data: { name: 'aa' } }
+  }
+
 })
 app.listen(3000)
--- node/test.js
+++ node/test.js
@@ -1,46 +1,7 @@
-// const getJSON = function (url) {
-//   return new Promise(function (resolve, reject) {
-//     const handler = function () {
-//       if (this.readyState !== 4) {
-//         return;
-//       }
-//       if (this.status === 200) {
-//         resolve(this.response);
-//       } else {
-//         reject(new Error(this.statusText));
-//       }
-//     };
-//     const client = new XMLHttpRequest();
-//     client.open("GET", url);
-//     client.onreadystatechange = handler;
-//     client.responseType = "json";
-//     client.setRequestHeader("Accept", "application/json");
-//     client.send();
+var proxy = new Proxy({}, {
+  get: function(target, property) {
+    return 35;
+  }
+});
 
-//   });
-
-// };
-
-// getJSON("../posts.json").then(function (json) {
-//   console.log('Contents: ' + json);
-// }, function (error) {
-//   console.error('出错了', error);
-// });
-
-function axios(method,url) {
-  return new Promise((resolve, reject) => {
-    var http = new XMLHttpRequest()
-    var handle = function (data) {
-      if (http.readyState == 4 && http.status ==200) {
-        resolve(http.response)
-      } else {
-        reject(new Error(http.statusText))
-      }
-    }
-    http.open(method, url);
-    http.onreadystatechange = handle
-    http.responseType = "json"
-    http.setRequestHeader('Accept', 'application/json')
-    http.send()
-  })
-}
+console.log(proxy.time) // 35
