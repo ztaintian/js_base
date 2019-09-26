7df9e0b87313086ad0cef4064ee76e8706574ee4
unknown <zhutiantian@ciicsh.local>
2019-09-26 15:51:26 +0800

    'hh'

--- node/test.js
+++ node/test.js
@@ -1,9 +1,46 @@
-var makeArray = function (arr) {
-  return Array.prototype.slice.call(arr, 0)
+// const getJSON = function (url) {
+//   return new Promise(function (resolve, reject) {
+//     const handler = function () {
+//       if (this.readyState !== 4) {
+//         return;
+//       }
+//       if (this.status === 200) {
+//         resolve(this.response);
+//       } else {
+//         reject(new Error(this.statusText));
+//       }
+//     };
+//     const client = new XMLHttpRequest();
+//     client.open("GET", url);
+//     client.onreadystatechange = handler;
+//     client.responseType = "json";
+//     client.setRequestHeader("Accept", "application/json");
+//     client.send();
+
+//   });
+
+// };
+
+// getJSON("../posts.json").then(function (json) {
+//   console.log('Contents: ' + json);
+// }, function (error) {
+//   console.error('出错了', error);
+// });
+
+function axios(method,url) {
+  return new Promise((resolve, reject) => {
+    var http = new XMLHttpRequest()
+    var handle = function (data) {
+      if (http.readyState == 4 && http.status ==200) {
+        resolve(http.response)
+      } else {
+        reject(new Error(http.statusText))
+      }
+    }
+    http.open(method, url);
+    http.onreadystatechange = handle
+    http.responseType = "json"
+    http.setRequestHeader('Accept', 'application/json')
+    http.send()
+  })
 }
-var obj = {
-  0: 1,
-  1: 2,
-  length: 2
-}
-console.log(makeArray(obj))
