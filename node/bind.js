c661389b8771f4a0a61df32823d254544b1f3a9a
unknown <zhutiantian@ciicsh.local>
2019-10-12 15:28:29 +0800

    'hh'

---
+++ node/bind.js
@@ -0,0 +1,18 @@
+Function.prototype.myBind = function (context) {
+  context = context || window
+  context.fn = this
+  var args = [...arguments].slice(1)
+  return function () {
+    context.fn(...args)
+    delete context.fn
+  }
+}
+
+var obj = {
+  name: 'gg'
+}
+var test = function (a,b,c) {
+  console.log(a,b,c)
+}
+// test.bind(obj,1,2,3)()
+test.myBind(obj,1,2,3)()
