1fe543746f7889565a974c480a859d71e633a1db
unknown <zhutiantian@ciicsh.local>
2019-10-21 15:58:43 +0800

    'gg'

---
+++ node/ajax.js
@@ -0,0 +1,19 @@
+var ajax = function (url) {
+  return new Promise((resolve, reject) => {
+    var http = new XMLHttpRequest()
+    http.open('GET', url)
+    http.onreadystatechange = function () {
+      if (this.readyState != 4) {
+        return
+      }
+      if (this.status === 200) {
+        resolve(this.response)
+      } else {
+        reject(new Error(this.statusText))
+      }
+    }
+  })
+}
+ajax().then((response) => {
+  console.log(response)
+})
