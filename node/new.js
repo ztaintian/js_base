e86b32778b730b4c899c03fd8378fc3d3bb63051
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2019-10-25 15:18:09 +0800

    'hh'

--- node/new.js
+++ node/new.js
@@ -1,14 +1,17 @@
-var myNew = function (obj, ...args) {
+var myNew = function (context,...args) {
   var _obj = {}
-  _obj._proto_ = obj.prototype
-  var result = obj.apply(_obj, args)
-  return typeof result === 'object' ? result : _obj
+  _obj.__proto__ = context.prototype
+  var result = context.apply(_obj,args)
+  return typeof result==='Object'?result:_obj
 }
 
-var test = function () {
-  this.a = '44444'
+function Test (name, age) {
+  this.name = name
+  this.age = age
 }
-
-var newTest = myNew(test)
-
-console.log(newTest.a)
+var myNewTest = myNew(Test, '小明', 19)
+console.log(myNewTest.name)
+console.log(myNewTest.age)
+var newTest = new Test('小天', 28)
+console.log(newTest.name)
+console.log(newTest.age)
--- node/object.js
+++ node/object.js
@@ -1,21 +1,23 @@
-var obj = [
-  {
-    aa: 1,
-    bb: 333
-  },
-  {
-    aa: 55,
-    bb: 000
-  }
-]
+// var obj = [
+//   {
+//     aa: 1,
+//     bb: 333
+//   },
+//   {
+//     aa: 55,
+//     bb: 000
+//   }
+// ]
 
-var tempObj = obj.map((item) => {
-  return {
-    ...item,
-    bb: (() => {
-      return 5555
-    })()
-  }
-})
+// var tempObj = obj.map((item) => {
+//   return {
+//     ...item,
+//     bb: (() => {
+//       return 5555
+//     })()
+//   }
+// })
 
-console.log(tempObj)
+// console.log(tempObj)
+var aa = Object.assign({},'123')
+console.log(aa)
