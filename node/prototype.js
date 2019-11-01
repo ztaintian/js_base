742146110561b778c50064140280b9f752f3ec3a
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2019-11-01 16:43:42 +0800

    'gg'

--- node/prototype.js
+++ node/prototype.js
@@ -11,39 +11,40 @@
 // 
 // 借用结构函数继承
 
-function Parent(name) {
-  this.name = name
-  this.say = function () {
-    console.log(this.name)
-  }
-}
-function Child(name, age) {
-  Parent.call(this, name)
-  this.age = age
-}
-
-var cc = new Child('father', 18)
-var bb = new Child('vvv', 88)
-cc.say()
-bb.say()
-console.log(cc.say === bb.say)
-
-// 组合继承
-
 // function Parent(name) {
 //   this.name = name
+//   this.say = function () {
+//     console.log(this.name)
+//   }
 // }
-// Parent.prototype.say = function () {
-//   console.log(this.name)
-// }
-// function Child(name,age) {
+// function Child(name, age) {
 //   Parent.call(this, name)
 //   this.age = age
 // }
-// Child.prototype = new Parent()
 
 // var cc = new Child('father', 18)
 // var bb = new Child('vvv', 88)
 // cc.say()
 // bb.say()
 // console.log(cc.say === bb.say)
+
+// 组合继承
+
+function Parent(name) {
+  this.name = name
+}
+Parent.prototype.say = function () {
+  console.log(this.name)
+}
+function Child(name, age) {
+  Parent.call(this, name)
+  this.age = age
+}
+Child.prototype = new Parent();
+Child.constructor = Child;
+
+var cc = new Child('father', 18)
+var bb = new Child('vvv', 88)
+cc.say()
+bb.say()
+console.log(cc.say === bb.say)
