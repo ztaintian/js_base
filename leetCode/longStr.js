7cfb56ec21e92793d9b6ae41a84da900b52c755b
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2019-11-06 16:25:04 +0800

    'gg'

--- leetCode/longStr.js
+++ leetCode/longStr.js
@@ -2,25 +2,20 @@
 // 输出: 3 
 // 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 
-var str = 'abcabcnm'
-
-var lengthOfLongestSubstring = function (s) {
-  var res = 0,
-    i = 0;
+var longStr = function (str) {
   var temp = [];
-  while (i < s.length) {
-    if (temp.indexOf(s[i]) === -1) {
-      temp.push(s[i]);
+  var i = 0;
+  while (i < str.length) {
+    if (temp.indexOf(str[i]) === -1) {
+      temp.push(str[i])
     } else {
       temp.shift();
       continue;
     }
-    res = Math.max(res, temp.length);
-    i++;
+    console.log('temp', temp)
+    i++
   }
-  console.log(temp)
-  return res;
-};
+}
 
-console.log(lengthOfLongestSubstring(str))
-
+var str = 'abcabcnmbb'
+longStr(str)
