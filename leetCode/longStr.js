db116b177a1046833f0b645a25fed352f54e9eff
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2019-11-06 14:23:02 +0800

    'hh'

--- leetCode/longStr.js
+++ leetCode/longStr.js
@@ -1,51 +1,26 @@
-// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-
-// 示例 1:
-
-// 输入: "abcadcvb"
+// 输入: "abcabcbb"
 // 输出: 3 
 // 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 
-var str = 'abcadcvb'
-function longStr(str) {
-  var arr = str.split('')
-  var obj = {}
-  var j = 0
-  var strArr = []
-  var nStr = ''
-  for (var i = 0; i < arr.length; i++) {
-    console.log('i', i)
-    var val = arr[i]
-    if (obj[val]) {
-      i = j
-      j++
-      obj = {}
-      strArr.push(nStr)
-      nStr = ''
-    } else {
-      obj[val] = true
-      nStr += val
-    }
-  }
-  console.log(strArr)
-}
-longStr(str)
+var str = 'abcabcnm'
 
-// i 0
-// i 1
-// i 2
-// i 3
-// i 1
-// i 2
-// i 3
-// i 4
-// i 5
-// i 2
-// i 3
-// i 4
-// i 5
-// i 3
-// i 4
-// i 5
-// i 6
-// i 7
+var lengthOfLongestSubstring = function (s) {
+  var res = 0,
+    i = 0;
+  var temp = [];
+  while (i < s.length) {
+    if (temp.indexOf(s[i]) === -1) {
+      temp.push(s[i]);
+    } else {
+      temp.shift();
+      continue;
+    }
+    res = Math.max(res, temp.length);
+    i++;
+  }
+  console.log(temp)
+  return res;
+};
+
+console.log(lengthOfLongestSubstring(str))
+
