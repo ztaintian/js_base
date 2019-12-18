9b6367ef7818effb44a7b5c58fe54bf63256d711
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2019-12-18 14:54:53 +0800

    'hh'

--- node/bind.js
+++ node/bind.js
@@ -1,18 +1,27 @@
-Function.prototype.myBind = function (context) {
-  context = context || window
+// Function.prototype.myBind = function (context) {
+//   context = context || window
+//   context.fn = this
+//   var args = [...arguments].slice(1)
+//   return function () {
+//     context.fn(...args)
+//     delete context.fn
+//   }
+// }
+
+// var obj = {
+//   name: 'gg'
+// }
+// var test = function (a,b,c) {
+//   console.log(a,b,c)
+// }
+// // test.bind(obj,1,2,3)()
+// test.myBind(obj,1,2,3)()
+Function.prototype.maBind = function (context) {
+  var context = context || window
   context.fn = this
-  var args = [...arguments].slice(1)
+  var args = [...arguments].slice(1);
   return function () {
-    context.fn(...args)
+    context.fn(args)
     delete context.fn
   }
 }
-
-var obj = {
-  name: 'gg'
-}
-var test = function (a,b,c) {
-  console.log(a,b,c)
-}
-// test.bind(obj,1,2,3)()
-test.myBind(obj,1,2,3)()
--- node/test.js
+++ node/test.js
@@ -1,14 +1,28 @@
-var fruit = [
-  { name: "Apple",      count: 13, },
-  { name: "Pear",       count: 12, },
-  { name: "Banana",     count: 12, },
-  { name: "Strawberry", count: 11, },
-  { name: "Cherry",     count: 11, },
-  { name: "Blackberry", count: 10, },
-  { name: "Pineapple",  count: 10, }
-];
-// Create our own sort criteria function:
-let my_sort = (a, b) => a.count - b.count;
-// Perform stable ES10 sort:
-let sorted = fruit.sort(my_sort);
-console.log(sorted);
+// 注意这里还是用到了 Map，原理和上文说的一致
+const copies = new Map()
+const objectTraps = {
+  get(target, key) {
+    if (key === MY_IMMER) return target
+    const data = copies.get(target) || target
+    return getProxy(data[key])
+  },
+  set(target, key, val) {
+    const copy = getCopy(target)
+    const newValue = getProxy(val)
+    // 这里的判断用于拿 proxy 的 target
+    // 否则直接 copy[key] = newValue 的话外部拿到的对象是个 proxy
+    copy[key] = isProxy(newValue) ? newValue[MY_IMMER] : newValue
+    return true
+  }
+}
+const getCopy = data => {
+  if (copies.has(data)) {
+    return copies.get(data)
+  }
+  const copy = Array.isArray(data) ? data.slice() : { ...data }
+  copies.set(data, copy)
+  return copy
+}
+const getCopy = (data) => {
+
+}
