44c11005c47729006c8882a2b43f6522b7f3558b
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2019-12-27 10:08:25 +0800

    'gg'

--- node/flat.js
+++ node/flat.js
@@ -9,6 +9,16 @@ var flat = function (arr) {
   })
   return tempArr
 }
-
+var flat = function () {
+  var tempArr = [];
+  arr.map((item) => {
+    if (Array.isArray(item)) {
+      tempArr = [...tempArr, ...flat(item)];
+    } else {
+      tempArr.push(item);
+    };
+  });
+  return tempArr;
+};
 var arr = [1,2,[3,4,[66,99],[44324,4242]]]
 console.log(flat(arr))
--- node/test.js
+++ node/test.js
@@ -1,1 +1,41 @@
-console.log(global)
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+// 你可以假设数组中无重复元素。
+
+// 示例 1:
+
+// 输入: [1,3,5,6], 5
+// 输出: 2
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/search-insert-position
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+var searchInsert = function (nums, target) {
+  var indexResult;
+  var result = nums.some((item, index) => {
+    if (item === target) {
+      indexResult = index;
+      return true;
+    } else {
+      return false;
+    }
+  })
+  if (!result) {
+    var result2 = nums.some((item, index) => {
+      if (item >= target) {
+        indexResult = index;
+        return true;
+      } else {
+        return false;
+      }
+    })
+    if (!result2) {
+      indexResult = nums.length;
+    }
+  } 
+  return  indexResult
+};
+var nums = [1,3,5,6];
+var target = 9
+console.log(searchInsert(nums, target))
