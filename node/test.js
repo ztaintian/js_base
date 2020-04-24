37bfe5da79865b25ef01ba6814ff4349368036af
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-04-24 10:40:19 +0800

    'gg'

--- node/test.js
+++ node/test.js
@@ -1,18 +1,9 @@
-function AA(o) {
-  function F() {}
-  F.prototype = o
-  return new F()
-}
-
-var obj = {
-  name: 'ww',
-  arr: [1,2]
-}
-
-var oneinstance = AA(obj)
-oneinstance.arr.push(88)
-oneinstance.name = 'rrr'
-var twoinstance = AA(obj)
-twoinstance.arr.push(99)
-twoinstance.name = 'twoinstance'
-console.log(oneinstance.arr, twoinstance.arr)
+// "use strict";
+var aa = (function f(num) {
+  if (num === 1) {
+    return num
+  } else {
+    return num * arguments.callee(num-1)
+  }
+})
+console.log(aa(5))
