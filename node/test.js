73737bbc8709a6ffb0a225847af9615662373796
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-07-03 16:19:17 +0800

    'ff'

--- node/test.js
+++ node/test.js
@@ -1,1 +1,17 @@
-[4].concat([1,23])
+var quickSort = function (arr) {
+  if (arr.length<2) {
+    return arr;
+  }
+  var tempArr = arr.splice(0, 1),
+  leftArr = [], rightArr = [];
+  arr.map((item) => {
+    if (item > tempArr[0]) {
+      rightArr.push(item);
+    } else {
+      leftArr.push(item);
+    }
+  })
+  return [...quickSort(leftArr),...tempArr, ...quickSort(rightArr)]
+}
+var arr = [42,5,7,8,5,23,54,6,7]
+console.log(quickSort(arr))
