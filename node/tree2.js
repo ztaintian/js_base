2b3fc33663d69ecc916eb14dbfc795cdf6be44ae
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-07-09 16:59:53 +0800

    'hh'

--- node/tree2.js
+++ node/tree2.js
@@ -12,17 +12,33 @@
 //   })(root);
 //   return result;
 // };
-
+// 首序排序
+// var preorderTraversal = function (root) {
+//   let stack = [root];
+//   let arr = [];
+//   while (stack.length > 0) {//循环迭代
+//     let node = stack.pop();
+//     node && arr.push(node.val); // node不为空时，向arr中推入节点值
+//     node && node.right && stack.push(node.right); //关键点：模拟栈，后入先出，故先压右节点
+//     node && node.left && stack.push(node.left); // 关键点：后入先出，后压左节点
+//   }
+//   return arr
+// };
+// 中序排序
 var preorderTraversal = function (root) {
-  let stack = [root];
-  let arr = [];
-  while (stack.length > 0) {//循环迭代
-    let node = stack.pop();
-    node && arr.push(node.val); // node不为空时，向arr中推入节点值
-    node && node.right && stack.push(node.right); //关键点：模拟栈，后入先出，故先压右节点
-    node && node.left && stack.push(node.left); // 关键点：后入先出，后压左节点
-  }
-  return arr
+  let result = [];
+  (function func(root) {
+    if (root)
+      
+    if (root && root.left) {
+      func(root.left);//递归
+    }
+    result.push(root.val);
+    if (root && root.right) {
+      func(root.right);//递归
+    }
+  })(root);
+  return result;
 };
 var root = {
   val: 2,
