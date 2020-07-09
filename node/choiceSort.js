f3c2adc77fbcd94679048565122185000c96c818
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-07-09 16:32:57 +0800

    'hh'

--- node/choiceSort.js
+++ node/choiceSort.js
@@ -1,17 +1,17 @@
-// 选择排序
-
-((arr) => {
+var choice = function (arr) {
   for (var i = 0; i < arr.length; i++) {
     var min = arr[i + 1], minIndex = i + 1;
     for (var j = i + 1; j < arr.length; j++) {
       if (arr[j] < min) {
-        min = arr[j];
         minIndex = j;
+        min = arr[minIndex];
       }
     }
     if (arr[i] > arr[minIndex]) {
       [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
     }
   }
-  console.log(arr)
-})([3235, 45, 567, 56, 7454])
+  return arr;
+}
+var arr = [13, 534, 632, 21, 3, 34]
+console.log(choice(arr))
