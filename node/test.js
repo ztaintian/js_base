3926f736cc542cd3113b50f90c7fc83eefe19407
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-07-09 14:19:56 +0800

    'kk'

--- node/test.js
+++ node/test.js
@@ -1,104 +1,67 @@
-let eventEmitter = {
-  // 缓存列表
-  list: {},
-  // 订阅
-  on(event, fn) {
-    let _this = this;
-    // 如果对象中没有对应的 event 值，也就是说明没有订阅过，就给 event 创建个缓存列表
-    // 如有对象中有相应的 event 值，把 fn 添加到对应 event 的缓存列表里
-    (_this.list[event] || (_this.list[event] = [])).push(fn);
-    return _this;
-  },
-  // 监听一次
-  once(event, fn) {
-    // 先绑定，调用后删除
-    let _this = this;
-    function on() {
-      _this.off(event, on);
-      fn.apply(_this, arguments);
-    }
-    on.fn = fn;
-    _this.on(event, on);
-    return _this;
-  },
-  // 取消订阅
-  off(event, fn) {
-    let _this = this;
-    let fns = _this.list[event];
-    // 如果缓存列表中没有相应的 fn，返回false
-    if (!fns) return false;
-    if (!fn) {
-      // 如果没有传 fn 的话，就会将 event 值对应缓存列表中的 fn 都清空
-      fns && (fns.length = 0);
-    } else {
-      // 若有 fn，遍历缓存列表，看看传入的 fn 与哪个函数相同，如果相同就直接从缓存列表中删掉即可
-      let cb;
-      for (let i = 0, cbLen = fns.length; i < cbLen; i++) {
-        cb = fns[i];
-        if (cb === fn || cb.fn === fn) {
-          fns.splice(i, 1);
-          break
-        }
+(() => {
+  //用多维数组代表题中示例的树，这个数组是按“前序遍历”后排序
+  let arrSouce = [6, [2, [-1], [3]], [3, [0]]];
+
+  /**
+   * 定义二叉树的结点。根结点和树的数据结构一样，
+   * 因此只需要定义一个结点(Node)的数据结构即可
+   */
+  class Node {
+    /**
+     * 构造函数
+     * @param {Number}  value 当前结点的值
+     * @param {Node}    left  左子结点
+     * @param {Node}    right 右子结点
+     */
+    constructor(value, left, right) {
+      if (value != undefined) {
+        this.value = value;
+        if (left != undefined) this.left = left;
+        if (right != undefined) this.right = right;
       }
     }
-    return _this;
-  },
-  // 发布
-  emit() {
-    let _this = this;
-    // 第一个参数是对应的 event 值，直接用数组的 shift 方法取出
-    let event = [].shift.call(arguments),
-      fns = _this.list[event];
-    // 如果缓存列表里没有 fn 就返回 false
-    if (!fns || fns.length === 0) {
-      return false;
-    }
-    // 遍历 event 值对应的缓存列表，依次执行 fn
-    fns.forEach(fn => {
-      fn.apply(_this, arguments);
-    });
-    return _this;
   }
-};
 
-function user1(content) {
-  console.log('用户1订阅了:', content);
-}
+  /**
+   * 创建一个树
+   * @param {Array} arr 一个代表二叉树的多维数组
+   */
+  function makeBTree(arr) {
+    if (arr) {
+      if (arr.length == 1) {
+        return new Node(arr[0]);
+      }
+      //递归，创建二叉树
+      return new Node(arr[0], makeBTree(arr[1]), makeBTree(arr[2]));
+    }
+  }
 
-function user2(content) {
-  console.log('用户2订阅了:', content);
-}
+  //创建示例中的二叉树，简洁多了！
+  let bTree = makeBTree(arrSouce);
 
-function user3(content) {
-  console.log('用户3订阅了:', content);
-}
+  console.log('bTree', bTree)
 
-function user4(content) {
-  console.log('用户4订阅了:', content);
-}
+  /**
+   * 主逻辑函数，与第一种解法里代码一样
+   * @param {Node}    node    树的结点
+   * @param {Number}  target  题目要求的目标值
+   */
+  function hasPathSum(node, target) {
+    //若根节点无子结点
+    if (!node.left && !node.right) {
+      //直接判断根结点额值是否等于target
+      return node.value == target;
+    }
+    //若有子结点
+    else {
+      //如有左结点 左侧递归
+      if (!!node.left)
+        return hasPathSum(node.left, /*关键*/ target - node.value);
+      //如有右结点 右侧递归
+      if (!!node.right)
+        return hasPathSum(node.right, /*关键*/ target - node.value);
+    }
+  }
 
-// 订阅
-eventEmitter.on('article1', user1);
-eventEmitter.on('article1', user2);
-eventEmitter.on('article1', user3);
-
-// 取消user2方法的订阅
-eventEmitter.off('article1', user2);
-
-eventEmitter.once('article2', user4)
-
-// 发布
-eventEmitter.emit('article1', 'Javascript 发布-订阅模式');
-eventEmitter.emit('article1', 'Javascript 发布-订阅模式');
-eventEmitter.emit('article2', 'Javascript 观察者模式');
-eventEmitter.emit('article2', 'Javascript 观察者模式');
-
-// eventEmitter.on('article1', user3).emit('article1', 'test111');
-
-/*>>
-  用户1订阅了: Javascript 发布-订阅模式
-  用户3订阅了: Javascript 发布-订阅模式
-  用户1订阅了: Javascript 发布-订阅模式
-  用户3订阅了: Javascript 发布-订阅模式
-  用户4订阅了: Javascript 观察者模式
-*/
+  console.log(hasPathSum(bTree, 7)); //>> true
+})();
---
+++ node/tree.js
@@ -0,0 +1,35 @@
+function Node(value, left, right) {
+  if (value) {
+    this.value = value;
+    if (left) this.left = left;
+    if (right) this.right = right;
+  }
+}
+
+let arrSouce = [6, [2, [-1], [3]], [3, [0]]];
+
+function Btree(arr) {
+  if (arr) {
+    if (arr.length === 1) {
+      return new Node(arr[0])
+    }
+    return new Node(arr[0], Btree(arr[1]), Btree(arr[2]))
+
+  }
+}
+var treeResult = Btree(arrSouce)
+
+function hasTrage(treeResult, traget) {
+  if (!treeResult.right && !treeResult.left) {
+    return treeResult.value === traget
+  } else {
+    if (treeResult.left) {
+      return hasTrage(treeResult.left, traget - treeResult.value)
+    }
+    if (treeResult.right) {
+      return hasTrage(treeResult.right, traget - treeResult.value)
+    }
+  }
+}
+
+console.log(hasTrage(treeResult, 7))
