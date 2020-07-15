0e308425062c45a21e15ac83191f4cfb1dc2508e
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-07-15 19:34:39 +0800

    'kk'

--- leetCode/solution .js
+++ leetCode/solution .js
@@ -1,19 +1,19 @@
 // 输入: nums1 = [1,2,2,1], nums2 = [2,2]
 // 输出: [2,2]
-// var solution = (arr1, arr2) => {
-//   var soluArr = [];
-//   arr1.map((item) => {
-//     var flag = arr2.some((item2) => {
-//       return item === item2
-//     })
-//     if (flag) {
-//       soluArr.push(item)
-//     }
-//   })
-//   return soluArr;
-// }
-// var arr1 = [1, 2, 2, 1], arr2 = [2, 2];
-// console.log(solution(arr1, arr2));
+var solution = (nums1, nums2) => {
+  var soluArr = [];
+  nums1.map((item) => {
+    var flag = nums2.some((item2) => {
+      return item === item2
+    })
+    if (flag && !soluArr.includes(item)) {
+      soluArr.push(item)
+    }
+  })
+  return soluArr;
+}
+var arr1 = [1, 2, 2, 1], arr2 = [2, 2];
+console.log(solution(arr1, arr2));
 
 // 存哈希表
 // const intersect = (nums1, nums2) => {
@@ -38,25 +38,25 @@
 // var arr1 = [1, 2, 2, 1], arr2 = [2, 2,1,5];
 // console.log(intersect(arr1,arr2))
 
-// 双指针 解法
-const intersect = (nums1, nums2) => {
-  nums1.sort((a, b) => a - b);
-  nums2.sort((a, b) => a - b); // 先排序，使得重复的元素相邻出现
-  const res = [];
-  let p1 = 0;                  // 两个指针
-  let p2 = 0;
-  while (p1 < nums1.length && p2 < nums2.length) { // 用&& 因为有一个越界了就不能找交集
-    if (nums1[p1] > nums2[p2]) {        // p1指向的数大，移动p2，期待遇到一样大的
-      p2++;
-    } else if (nums1[p1] < nums2[p2]) { // 类似
-      p1++;
-    } else {                   // 遇到相同的，推入res数组，两个指针同时移动考察下一个
-      res.push(nums1[p1]);
-      p1++;
-      p2++;
-    }
-  }
-  return res;
-};
-var arr1 = [1, 2, 2, 1], arr2 = [2, 2,1,5];
-console.log(intersect(arr1,arr2))
+// // 双指针 解法
+// const intersect = (nums1, nums2) => {
+//   nums1.sort((a, b) => a - b);
+//   nums2.sort((a, b) => a - b); // 先排序，使得重复的元素相邻出现
+//   const res = [];
+//   let p1 = 0;                  // 两个指针
+//   let p2 = 0;
+//   while (p1 < nums1.length && p2 < nums2.length) { // 用&& 因为有一个越界了就不能找交集
+//     if (nums1[p1] > nums2[p2]) {        // p1指向的数大，移动p2，期待遇到一样大的
+//       p2++;
+//     } else if (nums1[p1] < nums2[p2]) { // 类似
+//       p1++;
+//     } else {                   // 遇到相同的，推入res数组，两个指针同时移动考察下一个
+//       res.push(nums1[p1]);
+//       p1++;
+//       p2++;
+//     }
+//   }
+//   return res;
+// };
+// var arr1 = [1, 2, 2, 1], arr2 = [2, 2, 1, 5];
+// console.log(intersect(arr1, arr2))
