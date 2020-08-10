8919dbe03a06d80d9e0725e84563b0131ee8f7f0
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-08-10 18:53:18 +0800

    'jj'

--- node/new.js
+++ node/new.js
@@ -1,13 +1,18 @@
 var myNew = function (context,...args) {
   var _obj = {}
+  console.log('argsargsargs', args, context)
   _obj.__proto__ = context.prototype
   var result = context.apply(_obj,args)
-  return typeof result==='Object'?result:_obj
+  console.log('resultresultresult', typeof result, result, _obj)
+  return typeof result==='object'?result:_obj
 }
 
 function Test (name, age) {
   this.name = name
   this.age = age
+  return {
+    name:'daad'
+  }
 }
 var myNewTest = myNew(Test, '小明', 19)
 console.log(myNewTest.name)
@@ -16,9 +21,3 @@ var newTest = new Test('小天', 28)
 console.log(newTest.name)
 console.log(newTest.age)
 
-myNew = function () {
-  var _obj = {}
-  _obj.__proto__ = context.prototype
-  var result = context.apply(_obj,args)
-  return typeof result === 'Object'?result:_obj  
-}
