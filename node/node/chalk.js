fbe1326a5137aa95732c5b59099898a17fc83840
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-08-28 16:58:30 +0800

    'kk'

---
+++ node/node/chalk.js
@@ -0,0 +1,4 @@
+// npm install chalk --save-dev 
+// 修改console.log()文字颜色
+var chalk = require('chalk');
+console.log(chalk.red('fafaf'));
--- node/node/fs.js
+++ node/node/fs.js
@@ -1,3 +1,20 @@
 var fs = require('fs');
+// 同步读取
 var value = fs.readFileSync('index.txt', 'utf-8');
 console.log(value);
+// 异步读取
+fs.readFile('index.txt', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('data', data.toString());
+})
+
+// 异步打开文件
+console.log("准备打开文件！");
+fs.open('index.txt', 'r+', function (err, fd) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("文件打开成功！", fd);
+});
