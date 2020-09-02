c014e7e1cf3d2d96c4325a8a7424c00e51b78dd6
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-09-02 16:39:01 +0800

    'kk'

--- node/node/fs.js
+++ node/node/fs.js
@@ -1,20 +1,9 @@
-var fs = require('fs');
-// 同步读取
-var value = fs.readFileSync('index.txt', 'utf-8');
-console.log(value);
-// 异步读取
-fs.readFile('index.txt', (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('data', data.toString());
-})
+var fs = require("fs");
+var zlib = require('zlib');
 
-// 异步打开文件
-console.log("准备打开文件！");
-fs.open('index.txt', 'r+', function (err, fd) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log("文件打开成功！", fd);
-});
+// 压缩 input.txt 文件为 input.txt.gz
+fs.createReadStream('index.txt')
+  .pipe(zlib.createGzip())
+  .pipe(fs.createWriteStream('input.txt.gz'));
+  
+console.log("文件压缩完成。");
