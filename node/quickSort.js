26e9599b9937aa290976ea576c314a4d8189b116
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-10-14 19:53:05 +0800

    'll'

--- node/quickSort.js
+++ node/quickSort.js
@@ -17,3 +17,20 @@ var quickSort = function (arr) {
 }
 var arr = [333,4,32,64,2,4,67,8,79]
 console.log(quickSort(arr))
+
+var quickSort = function (arr) {
+  if (arr.length < 2) {
+    return arr
+  }
+  var tempNum = arr.splice(0,1)[0];
+  var leftArr = [];
+  var rightArr = [];
+  arr.map((item) => {
+    if (item > tempNum) {
+      rightArr.push(item);
+    } else {
+      leftArr.push(item);
+    }
+  })
+  return [...quickSort(left), ...[tempNum], ...quickSort(rightArr)]
+}
--- node/test.js
+++ node/test.js
@@ -1,18 +1,32 @@
-var obj = {
-  aa: 1,
-  bb: 2
+//懒加载代码实现
+var viewHeight = document.documentElement.clientHeight; //可视化区域的高度
+
+function lazyload() {
+  //获取所有要进行懒加载的图片
+  let eles = document.querySelectorAll('img[data-original][lazyload]'); //获取属性名中有data-original的
+  Array.prototype.forEach.call(eles, function (item, index) {
+    let rect;
+    if (item.dataset.original === '') {
+      return;
+    }
+
+    rect = item.getBoundingClientRect();
+
+    //图片一进入可视区，动态加载
+    if (rect.bottom >= 0 && rect.top < viewHeight) {
+      ! function () {
+        let img = new Image();
+        img.src = item.dataset.original;
+        img.onload = function () {
+          item.src = img.src;
+        }
+        item.removeAttribute('data-original');
+        item.removeAttribute('lazyload');
+      }();
+    }
+  })
 }
 
-var obj2 = {
-  bb: 55,
-  cc: 44
-}
+lazyload();
 
-var cc = {
-  ...obj,
-  ...obj2,
-  ...{
-    aa: (() => 999)()
-  }
-}
-console.log(cc)
+document.addEventListener('scroll', lazyload);
