8ee4c96d921ae5487c9564d318c123db973338f9
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-12-22 16:21:55 +0800

    'kk'

--- axios/index.js
+++ axios/index.js
@@ -1,3 +1,13 @@
-var axios = new axios({
-  
-})
+var name = "The Window";
+
+var object = {
+  name: "My Object",
+  getNameFunc: function () {
+    var that = this
+    return function () {
+      return that.name;
+    };
+  }
+};
+
+console.log(object.getNameFunc()());
---
+++ promise-all.js
@@ -0,0 +1,41 @@
+function promiseAll(promises) {
+  return new Promise(function (resolve, reject) {
+    if (!Array.isArray(promises)) {
+      throw new TypeError(`argument must be a array`)
+    }
+    var resolvedCounter = 0;
+    var promiseNum = promises.length;
+    var resolvedResult = [];
+    for (let i = 0; i < promiseNum; i++) {
+      Promise.resolve(promises[i]).then(value => {
+        resolvedCounter++;
+        resolvedResult[i] = value;
+        if (resolvedCounter == promiseNum) {
+          return resolve(resolvedResult)
+        }
+      }, error => {
+        return reject(error)
+      })
+    }
+  })
+}
+
+// test
+let p1 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve(1)
+  }, 1000)
+})
+let p2 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve(2)
+  }, 2000)
+})
+let p3 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve(3)
+  }, 3000)
+})
+promiseAll([p3, p1, p2]).then(res => {
+  console.log(res) // [3, 1, 2]
+})
