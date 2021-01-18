98baa2e3d718ae7a431f5252913963b0d4c86aba
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-01-18 13:56:28 +0800

    ll

--- node/bubbleSort.js
+++ node/bubbleSort.js
@@ -1,6 +1,6 @@
 var bubbleSort = function (arr) {
-  for (var i=0; i<arr.length; i++) {
-    for (var j=i;j<arr.length;j++) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i; j < arr.length; j++) {
       if (arr[i] > arr[j]) {
         [arr[i], arr[j]] = [arr[j], arr[i]]
       }
@@ -8,5 +8,19 @@ var bubbleSort = function (arr) {
   }
   return arr
 }
-var arr = [43,55,775,565,66]
+var arr = [43, 55, 775, 565, 66]
 console.log(bubbleSort(arr))
+
+var bubble = function (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubble(arr));
