70d362ee473139935be1ce613e6b975cbb9bdf07
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-03-05 11:08:15 +0800

    'party'

--- node/test.js
+++ node/test.js
@@ -1,11 +1,14 @@
-const testArr = [1, 2, 3, 4]
-Array.prototype.reduceFilter = function (callback) {
-  return this.reduce((acc, cur, index, array) => {
-    if (callback(cur, index, array)) {
-      acc.push(cur)
-    }
-    return acc
-  }, [])
+function reload(name: string): void
+
+function reload(name: string, age ? : number): void
+
+function reload(name: any, age ? number): any {
+  if (age) {
+    console.log(`我的名字是: ${name}, 今年${age}岁！！`);
+  } else {
+    console.log(`大家好，我的名字是: ${name}`);
+  }
 }
-testArr.reduceFilter(item => item % 2 == 0) // 过滤出偶数项。
-// [2, 4]
+
+reload('Clearlove'); // 大家好，我的名字是Clearlove
+reload('Clearlove', 18); // 我的名字是Clearlove， 今年18岁！！
