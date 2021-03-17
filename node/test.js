4feb7480cfa9d5f022d56111ac19079608252f10
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-03-17 10:20:26 +0800

    'll'

--- node/test.js
+++ node/test.js
@@ -1,8 +1,17 @@
-function reverseInt(intNumber) {
-  console.log('intNumber % 10', intNumber, intNumber % 10)
-  if (!(intNumber % 10)) {
-    return ''
-  }
-  return (intNumber % 10).toString() + reverseInt(parseInt(intNumber / 10))
+function Promi(executor) {
+  let _this = this;
+  _this.$$status = 'pending';
+  _this.failCallBack = undefined;
+  _this.successCallback = undefined;
+  _this.error = undefined;
+  setTimeout(() => {
+    try {
+      executor(_this.onResolve.bind(_this),_this.onReject.bind(_this))
+    } catch (e) {
+      _this.error = e;
+      if (_this.callBackDefer && _this.callBackDefer.fail) {
+        _this.callBackDefer.fail(e)
+      }
+    }
+  })
 }
-console.log(reverseInt(12345))
