0d1c648359971493bb669291c39abd97e89a42e4
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-03-26 16:39:03 +0800

    'll'

--- node/eventloop.js
+++ node/eventloop.js
@@ -1,10 +1,10 @@
 // js 宏任务和微任务 https://www.cnblogs.com/ckAng/p/11133643.html
 
 console.log(1)
-setTimeout(function() {
+setTimeout(function () { // 宏任务
   console.log(2)
 }, 0)
-new Promise(function (resolve) {
+new Promise(function (resolve) { //微任务
   console.log(3)
   var i = 0
   while (i < 9) {
@@ -13,8 +13,8 @@ new Promise(function (resolve) {
   console.log(i);
   resolve()
   console.log(4)
-}).then(function() {
-  console.log(5)
+}).then(function () {
+  console.log(5) //
 })
 console.log(6)
 // 1
