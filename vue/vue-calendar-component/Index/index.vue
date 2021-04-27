8dcef6400829b64e721890fc8e9d1cd01c8b5ebb
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-04-27 16:24:01 +0800

    'll'

---
+++ vue/vue-calendar-component/Index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+    <Calendar></Calendar>
+  </div>
+</template>
+
+<script>
+  import Calendar from './index';
+  export default {
+    components: {
+      Calendar
+    },
+    data() {
+      return {};
+    },
+    mounted() {},
+    methods: {}
+  };
+
+</script>
+<style lang="scss">
+</style>
