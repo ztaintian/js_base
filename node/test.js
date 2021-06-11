dfb9cfe3bab454fbb7f37cc5e341add3acd88c72
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-06-11 15:06:28 +0800

    'kk'

--- node/test.js
+++ node/test.js
@@ -1,1 +1,15 @@
-var a= 'AllsalaryList'.toLocaleUpperCase()
+function deepCopy(obj) {
+  if (!obj || typeof obj != 'object') {
+    return {};
+  }
+  let newObject = Array.isArray(obj) ? [] : {};
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      newObject[key] = obj[key] && typeof obj[key] === 'object' ? deepCopy(obj[key]) : obj[key];
+    }
+  }
+  return newObject;
+}
+function deepCope() {
+  
+}
