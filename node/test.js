a162640c5f00a4874134d124ac44935c67c84b92
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-09-03 14:14:38 +0800

    'll'

--- node/test.js
+++ node/test.js
@@ -1,12 +1,19 @@
-var arr = [
-  {
-    money: 1,
-  },
-  {
-    money: 2,
-  },
-];
-var cc = arr.reduce((pre, cur) => {
-  return pre + cur.money;
-}, 0);
-console.log(cc);
+var arr = [323, 435, 6, 4, 3, 24, 342];
+function quickSort(arr) {
+  var left = [],
+    right = [],
+    tempValue;
+  if (arr.length < 2) {
+    return arr;
+  }
+  tempValue = arr.splice(0, 1)[0];
+  arr.map((item) => {
+    if (item < tempValue) {
+      left.push(item);
+    } else {
+      right.push(item);
+    }
+  });
+  return [...quickSort(left), tempValue, ...quickSort(right)];
+}
+console.log(quickSort(arr));
