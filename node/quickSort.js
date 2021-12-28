5482b3c80c5d3054f886d67f0ed97ec3b24aa3c7
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2021-12-28 16:10:04 +0800

    'xiugai'

--- node/quickSort.js
+++ node/quickSort.js
@@ -1,39 +1,54 @@
-var quickSort = function (arr) {
-  if (arr.length < 2) {
-    return arr
-  }
-  var tempNum = arr.splice(0, 1)[0];
-  console.log('arr', arr, 'tempNum', tempNum)
-  var leftArr = [];
-  var rightArr = [];
-  arr.map((item) => {
-    if (item > tempNum) {
-      rightArr.push(item);
-    } else {
-      leftArr.push(item);
-    }
-  })
-  return [...quickSort(leftArr), ...[tempNum], ...quickSort(rightArr)]
-}
-var arr = [333,4,32,64,2,4,67,8,79]
-console.log(quickSort(arr))
-
-var quickSort = function (arr) {
-  if (arr.length < 2) {
-    return arr
-  }
-  var tempNum = arr.splice(0,1)[0];
-  var leftArr = [];
-  var rightArr = [];
-  arr.map((item) => {
-    if (item > tempNum) {
-      rightArr.push(item);
-    } else {
-      leftArr.push(item);
-    }
-  })
-  return [...quickSort(left), ...[tempNum], ...quickSort(rightArr)]
-}
+// var quickSort = function (arr) {
+//   if (arr.length < 2) {
+//     return arr
+//   }
+//   var tempNum = arr.splice(0, 1)[0];
+//   console.log('arr', arr, 'tempNum', tempNum)
+//   var leftArr = [];
+//   var rightArr = [];
+//   arr.map((item) => {
+//     if (item > tempNum) {
+//       rightArr.push(item);
+//     } else {
+//       leftArr.push(item);
+//     }
+//   })
+//   return [...quickSort(leftArr), ...[tempNum], ...quickSort(rightArr)]
+// }
+// var arr = [333,4,32,64,2,4,67,8,79]
+// console.log(quickSort(arr))
+// var qucikSort = function () {
+//   if (arr.length < 2) {
+//     return arr
+//   }
+//   var tempNum = arr.splice(0,1)[0];
+//   var leftArr = [];
+//   var rightArr = [];
+//   arr.map((item) => {
+//     if (item > tempNum) {
+//       rightArr.push(item)
+//     } else {
+//       leftArr.push(item)
+//     }
+//   })
+//   return [...quickSort(left), ...[tempNum], ...quickSort(rightArr)]
+// }
+// var quickSort = function (arr) {
+//   if (arr.length < 2) {
+//     return arr
+//   }
+//   var tempNum = arr.splice(0,1)[0];
+//   var leftArr = [];
+//   var rightArr = [];
+//   arr.map((item) => {
+//     if (item > tempNum) {
+//       rightArr.push(item);
+//     } else {
+//       leftArr.push(item);
+//     }
+//   })
+//   return [...quickSort(left), ...[tempNum], ...quickSort(rightArr)]
+// }
 
 var quickSort = function (arr) {
   if (arr.length < 2) {
@@ -49,4 +64,9 @@ var quickSort = function (arr) {
       leftArr.push(item)
     }
   })
+  return [...quickSort(leftArr), ...[tempNum], ...quickSort(rightArr)]
 }
+
+var arr = [12,3,4,5,6,78,76,5665];
+console.log(quickSort(arr))
+
--- node/test.js
+++ node/test.js
@@ -1,1 +1,30 @@
-var arr = [1,2,34,5,66,7,88,88];
+
+
+var addTwoNumbers = function(l1, l2) {
+  // 创建一个虚拟头节点，申明一个cur节点指向虚拟头节点
+  let pre = new ListNode(0),cur = pre;
+  let carry = 0; // 申明一个计数值
+  while(l1 || l2){
+    // 获取链表的值
+    let x = l1 === null ? 0 : l1.val;
+    let y = l2 === null ? 0 : l2.val;
+    let sum = x + y + carry;
+    // 超出10的话，记录1，加入到下次的运算中
+    carry = Math.floor(sum / 10); // 取模
+    sum = sum % 10; // 取余
+    cur.next = new ListNode(sum); // 给cur下个节点赋值
+    cur = cur.next; // 节点往后走一位
+    if(l1 != null){
+      l1 = l1.next; // 循环的节点不为null，就往后走一位
+    }
+    if(l2 != null){ // 循环的节点不为null，就往后走一位
+      l2 = l2.next;
+    }
+  }
+  if(carry == 1){ // 最后判断计数值是否大于1，大于1，链表往后加一位，且值为1
+    cur.next = new ListNode(carry);
+  }
+  return pre.next;
+};
+
+console.log(addTwoNumbers([1,2,3], [2,3,4]))
