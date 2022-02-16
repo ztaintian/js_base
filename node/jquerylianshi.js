da9eea91feecd2b41f9ffa1ed625d7b80061d111
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2022-02-16 15:21:19 +0800

    'll'

---
+++ node/jquerylianshi.js
@@ -0,0 +1,18 @@
+function Demo () {}
+
+Demo.prototype = {
+  getName(name) {
+    this.name = name
+    return this;
+  },
+  getAge(age) {
+    this.age = age
+    return this;
+  }
+}
+
+function D() {
+  return new Demo();
+}
+
+console.log(D().getName('fff').name)
