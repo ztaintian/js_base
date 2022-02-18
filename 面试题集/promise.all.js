7fa2827aa36f7235b509e65cd9b96c0f2840ab29
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2022-02-18 09:56:00 +0800

    'll'

--- 面试题集/promise.all.js
+++ 面试题集/promise.all.js
@@ -1,3 +1,5 @@
+// promise执行顺序与返回顺序
+
 function promiseA() {
   return new Promise((resolve,reject) => {
     setTimeout(() => {
@@ -20,3 +22,6 @@ function promiseB() {
 Promise.all([promiseA(),promiseB()]).then(reslove => {
   console.log(reslove)
 })
+
+
+// promise.all返回错直接resolve错误信息，不要reject，导致成功的信息也不能获取到
