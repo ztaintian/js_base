dc54508603e541465d7e982198cc08651815c28c
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2022-06-17 10:45:47 +0800

    'll'

--- node/test.js
+++ node/test.js
@@ -1,3 +1,13 @@
-Function.prototype.myCall = function() {
-  var fn = this;
-}
+setTimeout(function(){
+  console.log(1);
+});
+new Promise(function(resolve){		    
+  console.log(2);
+  resolve();
+}).then(function(){		    
+  console.log(3);
+}).then(function(){
+  console.log(4)
+}); 		
+console.log(5);
+// 2/5/3/4/1
