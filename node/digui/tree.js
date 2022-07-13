44769ffa5c4fb4e2ac74508f9007e5d323459f19
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2022-07-13 15:39:11 +0800

    'll'

--- node/digui/tree.js
+++ node/digui/tree.js
@@ -3,25 +3,38 @@ var data = [
   { id: 2, name: "菜单申请", pid: 1 },
   { id: 3, name: "信息申请", pid: 1 },
   { id: 4, name: "模块记录", pid: 2 },
-  { id: 5, name: "系统设置", pid: 2 },
   { id: 6, name: "权限管理", pid: 5 },
   { id: 7, name: "用户角色", pid: 6 },
+  { id: 5, name: "系统设置", pid: 0 },
+
   { id: 8, name: "菜单设置", pid: 6 },
 ];
 
-function tree(arr, pid, result) {
-  arr.map((item) => {
-    if (item.pid === pid) {
-      var newItem = { ...item, child: [] };
-      result.push(newItem);
-      tree(arr, item.id, item.child);
-    }
+function toTree(data) {
+  // 删除 所有 children,以防止多次调用
+  data.forEach(function (item) {
+      delete item.children;
   });
-}
 
-function arrToTree(arr,pid) {
-  var result = [];
-  tree(arr, 0, result);
-  return result;
+  // 将数据存储为 以 id 为 KEY 的 map 索引数据列
+  var map = {};
+  data.forEach(function (item) {
+      map[item.id] = item;
+  });
+//        console.log(map);
+  var val = [];
+  data.forEach(function (item) {
+      // 以当前遍历项，的pid,去map对象中找到索引的id
+      var parent = map[item.pid];
+      // 好绕啊，如果找到索引，那么说明此项不在顶级当中,那么需要把此项添加到，他对应的父级中
+      if (parent) {
+          (parent.children || ( parent.children = [] )).push(item);
+      } else {
+          //如果没有在map中找到对应的索引ID,那么直接把 当前的item添加到 val结果集中，作为顶级
+          val.push(item);
+      }
+  });
+  return val;
 }
-arrToTree(data,0);
+console.log(toTree(data))
+
