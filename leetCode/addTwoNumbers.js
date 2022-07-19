14d4f7a6fe1ea291fba91cc1456eadc11d6fbdd3
ztaintian <243752773@qq.com>
2022-07-19 16:57:34 +0800

    ll

--- leetCode/addTwoNumbers.js
+++ leetCode/addTwoNumbers.js
@@ -11,17 +11,29 @@
 // 原因：342 + 465 = 807
 
 var addTwoNumbers = function (l1, l2) {
-  let sum = 0
-  let head = {}
-  let cur = head
-
-  while (l1 || l2 || sum) {
-    sum += (l1 && l1.val) + (l2 && l2.val)
-    cur = cur.next = new ListNode(sum % 10)
-
-    l1 = l1 && l1.next
-    l2 = l2 && l2.next
-    sum = Math.floor(sum / 10)
+  let head = null,
+    tail = null
+  let carry = 0
+  while (l1 || l2) {
+    const n1 = l1 ? l1.val : 0
+    const n2 = l2 ? l2.val : 0
+    const sum = n1 + n2 + carry
+    if (!head) {
+      head = tail = new ListNode(sum % 10)
+    } else {
+      tail.next = new ListNode(sum % 10)
+      tail = tail.next
+    }
+    carry = Math.floor(sum / 10)
+    if (l1) {
+      l1 = l1.next
+    }
+    if (l2) {
+      l2 = l2.next
+    }
   }
-  return head.next
-};
+  if (carry > 0) {
+    tail.next = new ListNode(carry)
+  }
+  return head
+}
