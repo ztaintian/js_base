a663a7fb0f98a683d982723a3d1990e10fd72d1a
ztaintian <243752773@qq.com>
2022-11-15 09:10:03 +0800

    ll

--- node/new.js
+++ node/new.js
@@ -1,29 +1,27 @@
 var myNew = function (context, ...args) {
   var _obj = {};
-  console.log("argsargsargs", args, context);
   _obj.__proto__ = context.prototype;
-  var result = context.apply(_obj, args);
-  console.log("resultresultresult", typeof result, result, _obj);
-  return typeof result === "object" ? result : _obj;
+  var result = context.call(_obj, args);
+  console.log('result', result, _obj)
+  return _obj
+  // return typeof result === "object" ? result : result;
 };
 
 function Test(name, age) {
   this.name = name;
   this.age = age;
-  return {
-    name: "daad",
-  };
+  // return {
+  //   name: "daad",
+  // };
 }
 var myNewTest = myNew(Test, "小明", 19);
-console.log("dadada", myNewTest.name);
-console.log("dadadad", myNewTest.age);
-var newTest = new Test("小天", 28);
-console.log(newTest.name);
-console.log(newTest.age);
+// var newTest = new Test("小天", 28);
+console.log(myNewTest.name);
+console.log(myNewTest.age);
 
-function newNew(obj, ...args) {
-  var _obj = {};
-  _obj.__proto__ = obj.prototype;
-  var result = obj.apply(_obj, args);
-  return typeof result === "object" ? result : _obj;    
-}
+// function newNew(obj, ...args) {
+//   var _obj = {};
+//   _obj.__proto__ = obj.prototype;
+//   var result = obj.apply(_obj, args);
+//   return typeof result === "object" ? result : _obj;    
+// }
--- node/test.js
+++ node/test.js
@@ -1,33 +1,15 @@
-class LoginFrame {
-  static instance = null
-  constructor(state) {
-    this.state = state
-  }
-  show() {
-    if (this.state === 'show') {
-      console.log('登录框已显示')
-      return
-    }
-    this.state = 'show'
-    console.log('登录框展示成功')
-  }
-  hide() {
-    if (this.state === 'hide') {
-      console.log('登录框已隐藏')
-      return
-    }
-    this.state = 'hide'
-    console.log('登录框隐藏成功')
-  }
-  // 通过静态方法获取静态属性instance上是否存在实例，如果没有创建一个并返回，反之直接返回已有的实例
-  static getInstance(state) {
-    if (!this.instance) {
-      this.instance = new LoginFrame(state)
-    }
-    return this.instance
-  }
+// function Person(){}
+// Person.prototype.drink = function() {
+//   console.log('喝东西')
+// }
+
+// let personA = new Person('张三')
+// Person.drink() // 输出：喝东西
+
+function myNew(fn, args) {
+  var obj = {}
+  obj._proto_ = fn.prototype
+  fn.apply(obj, args)
+  delete fn
+  return typeof obj === 'object'?obj:
 }
-const p1 = LoginFrame.getInstance('show')
-const p2 = LoginFrame.getInstance('hide')
-p1.show()
-p1.hide()
