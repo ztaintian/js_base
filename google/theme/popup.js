2d8814b39741638468bf9648dcaa3218d3dde6ec
ztaintian <243752773@qq.com>
2022-11-18 14:09:30 +0800

    ddd

---
+++ google/theme/popup.js
@@ -0,0 +1,25 @@
+// Initialize button with user's preferred color
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+// 单击按钮时，将 setPageBackgroundColor 注入当前页面
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  console.log('tab', tab)
+  if (tab.url?.startsWith("chrome://")) return undefined;
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// 此函数的主体将作为内容脚本执行
+// 当前页面
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
---
+++ google/theme/service-worker.js
@@ -0,0 +1,7 @@
+
+let color = '#000';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
