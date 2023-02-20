4e7eea25d7ee0014ec615daa7a9229f49bdc4329
ztaintian <243752773@qq.com>
2023-02-20 15:15:52 +0800

    kk

--- node/test1.js
+++ node/test1.js
@@ -1,13 +1,67 @@
-function curry(protocol) {
-  return function (hostname, pathname) {
-    return `${protocol}${hostname}${pathname}`;
+// 源码位置：src/core/components/keep-alive.js
+export default {
+  name: 'keep-alive',
+  abstract: true,
+  props: {
+    include: patternTypes,
+    exclude: patternTypes,
+    max: [String, Number]
+  },
+  created() {
+    this.cache = Object.create(null)
+    this.keys = []
+  },
+  destroyed() {
+    for (const key in this.cache) {
+      pruneCacheEntry(this.cache, key, this.keys)
+    }
+  },
+  mounted() {
+    this.$watch('include', val => {
+      pruneCache(this, name => matches(val, name))
+    })
+    this.$watch('exclude', val => {
+      pruneCache(this, name => !matches(val, name))
+    })
+  },
+  render() {
+    const slot = this.$slots.default
+    const vnode = getFirstComponentChild(slot)
+    const componentOptions = vnode && vnode.componentOptions
+    if (componentOptions) {
+      // check pattern
+      const name = getComponentName(componentOptions)
+      const { include, exclude } = this
+      if (
+        // not included
+        (include && (!name || !matches(include, name))) ||
+        // excluded
+        (exclude && name && matches(exclude, name))
+      ) {
+        return vnode
+      }
+
+      const { cache, keys } = this
+      const key = vnode.key == null
+        // same constructor may get registered as different local components
+        // so cid alone is not enough (#3269)
+        ? componentOptions.Ctor.cid + (componentOptions.tag ? `::${componentOptions.tag}` : '')
+        : vnode.key
+      if (cache[key]) {
+        vnode.componentInstance = cache[key].componentInstance
+        // make current key freshest
+        remove(keys, key)
+        keys.push(key)
+      } else {
+        cache[key] = vnode
+        keys.push(key)
+        // prune oldest entry
+        if (this.max && keys.length > parseInt(this.max)) {
+          pruneCacheEntry(cache, keys[0], keys, this._vnode)
+        }
+      }
+      vnode.data.keepAlive = true
+    }
+    return vnode || (slot && slot[0])
   }
 }
-
-const url_curry = curry('https://');
-
-const url1 = url_curry('www.baidu.com', '/hasa');
-const url2 = url_curry('www.zhihu.com', '/saandsa');
-const url3 = url_curry('www.segmentfault.com', '/hasak');
-
-console.log(url1, url2, url3)
