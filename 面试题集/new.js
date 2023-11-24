5d52db20d0201a9e3f4bdf61ecc4b19953a98671
ztaintian <243752773@qq.com>
2023-11-24 16:54:38 +0800

    ll

--- 面试题集/new.js
+++ 面试题集/new.js
@@ -4,3 +4,29 @@ function myNew(fn) {
   obj.__proto__ = fn.prototype;
   fn.apply(obj, args)
 }
+
+
+
+
+
+
+Function.prototype.myCall = function (obj,...args) {
+  var _obj = {}
+  obj.fn = this;
+  obj.fn(this,...args);
+  delete obj.fn;
+}
+
+
+
+
+
+
+
+
+// function myNew(fn, ...args) {
+//   let obj = {};
+//   obj.__proto__ = fn.prototype;
+//   let result = fn.apply(obj, args);
+//   return typeof result === 'object' ? result : obj
+// }
--- 面试题集/promise手写.js
+++ 面试题集/promise手写.js
@@ -17,7 +17,7 @@ class WPromise {
       this.reason = reason;
     }
   }
-  then(onFulfilled,onRejected) {
+  then(onFulfilled, onRejected) {
     if (this.state === 'fulfilled') {
       onFulfilled(this.value);
     }
@@ -27,9 +27,39 @@ class WPromise {
   }
 }
 
-var aa = new WPromise((resolve,reject) => {
+var aa = new WPromise((resolve, reject) => {
   resolve('aaa')
 });
 aa.then((cc) => {
-console.log(cc)
+  console.log(cc)
 })
+
+
+class myPromise {
+  constructor(executor) {
+    this.reason = '';
+    this.value = '';
+    this.state = 'pending';
+    executor(this._resolve.bind(this), this._reject.bind(this));
+  }
+  _resolve(value) {
+    if (this.state === 'pending') {
+      this.state = 'resolve';
+      this.value = value;
+    }
+  }
+  _reject(reason) {
+    if (this.state === 'reject') {
+      this.state = 'reject';
+      this.value = reason;
+    }
+  }
+  then(resolveFn, rejectFn) {
+    if (this.state === 'resolve') {
+      resolveFn && resolveFn(this.value)
+    }
+    if (this.state === 'reject') {
+      rejectFn && rejectFn(this.value);
+    }
+  }
+}
